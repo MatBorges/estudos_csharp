27e0be1ed1ffd4180a8fae086982a0ad35d75904
Mateus Borges Oliveira de Araujo <mateus.araujo@notacontrol.matriz>
2022-06-22 14:18:05 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace estudos_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto pc = new Produto();
+
+            pc.Nome = Console.ReadLine();
+            pc.Preco = double.Parse(Console.ReadLine());
+            pc.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Nome: {pc.Nome}");            
+
+        }
+    }
+}
